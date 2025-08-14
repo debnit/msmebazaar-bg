6def32a1fe1478a3c98907611b7f63776dc9acb7
debnit <debasis.nitrkl@gmail.com>
2025-08-15 00:13:31 +0530

    Create index.ts

---
+++ api-gateway/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import routes from "./routes/serviceRoutes";
+import { errorHandler } from "./middlewares/errorHandler";
+
+const app = express();
+
+app.use("/api", routes);
+app.use(errorHandler);
+
+app.listen(process.env.GATEWAY_PORT || 6000, () => {
+  console.log(`API Gateway running on port ${process.env.GATEWAY_PORT || 6000}`);
+});
