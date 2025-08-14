ee47add7756e6cb141ada4b8296783c1b5169e6f
debnit <debasis.nitrkl@gmail.com>
2025-08-15 00:31:46 +0530

    Update serviceRoutes.ts

--- api-gateway/src/routes/serviceRoutes.ts
+++ api-gateway/src/routes/serviceRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { verifyJwt } from "../middlewares/auth";
 import { correlationId } from "../middlewares/correlation";
 import { limiter } from "../middlewares/rateLimiter";
+import { logRequests } from "../utils/logger";
 import { requireFeature } from "../middlewares/requireFeature";
 import { createServiceProxy } from "../services/proxyFactory";
 import { servicesConfig } from "../config/services";
@@ -13,28 +14,29 @@ function createServiceProxyRoute(serviceName: string, serviceUrl: string, authRe
   const proxyBreaker = createServiceProxy(serviceName, serviceUrl);
 
   const featureGate = (req: any, res: any, next: any) => {
-    const relPath = `${serviceName}${req.path}`.replace(/^\/+/, "").toLowerCase();
-    const feature = featureServiceMap[relPath];
+    const key = `${serviceName}${req.path}`.replace(/^\/+/, "").toLowerCase();
+    const feature = featureServiceMap[key];
     if (feature) {
       return requireFeature(feature)(req, res, next);
     }
     next();
   };
 
-  const chain: any[] = [correlationId, limiter];
-  if (authRequired) chain.push(verifyJwt);
-  chain.push(featureGate);
-  chain.push((req: any, res: any, next: any) =>
-    proxyBreaker.fire(req, res).catch(next)
-  );
+  const chain: any[] = [
+    correlationId,
+    logRequests,
+    limiter,
+    ...(authRequired ? [verifyJwt] : []),
+    featureGate,
+    (req: any, res: any, next: any) => proxyBreaker.fire(req, res).catch(next)
+  ];
 
   router.use(`/${serviceName}`, ...chain);
 }
 
 createServiceProxyRoute("auth", servicesConfig.auth, false);
-
-Object.entries(servicesConfig).forEach(([name, url]) => {
-  if (name !== "auth") createServiceProxyRoute(name, url, true);
-});
+Object.entries(servicesConfig)
+  .filter(([name]) => name !== "auth")
+  .forEach(([name, url]) => createServiceProxyRoute(name, url, true));
 
 export default router;
