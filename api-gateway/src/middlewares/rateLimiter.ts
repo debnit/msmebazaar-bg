1dcd9ef4fed8cb0421467e4b5a69f93ed796741b
debnit <debasis.nitrkl@gmail.com>
2025-08-15 00:37:49 +0530

    Create rateLimiter.ts

---
+++ api-gateway/src/middlewares/rateLimiter.ts
@@ -0,0 +1,8 @@
+// src/middlewares/rateLimiter.ts
+import rateLimit from "express-rate-limit";
+
+export const limiter = rateLimit({
+  windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS || "900000", 10), // default 15 min
+  max: parseInt(process.env.RATE_LIMIT_MAX || "1000", 10),
+  message: { success: false, message: "Too many requests from this IP" }
+});
