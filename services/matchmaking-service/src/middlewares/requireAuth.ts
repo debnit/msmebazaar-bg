a827e39728e92537a9282a62ea888af351c0a12f
debnit <debasis.nitrkl@gmail.com>
2025-09-05 00:13:23 +0000

    fixed matchmaking service

--- services/matchmaking-service/src/middlewares/requireAuth.ts
+++ services/matchmaking-service/src/middlewares/requireAuth.ts
@@ -1,4 +1,3 @@
 import { jwtMw } from "@msmebazaar/shared/auth";
 import { Config } from "../config/env";
-
 export default jwtMw(Config.jwtSecret);
--- services/matchmaking-service/src/routes/matchmaking.routes.ts
+++ services/matchmaking-service/src/routes/matchmaking.routes.ts
@@ -3,13 +3,13 @@ import  requireAuth  from "../middlewares/requireAuth";
 import {  requireRole } from "@msmebazaar/shared/middleware/auth";
 import { createMatchController, getMatchesController } from "../controllers/matchmaking.controller";
 
+
 const router = Router();
 
-router.post("/matchmaking", jwtMw, createMatchController);
-router.get("/matchmaking", jwtMw, getMatchesController);
+router.post("/matchmaking", requireAuth, createMatchController);
+router.get("/matchmaking", requireAuth, getMatchesController);
 
 // Expose special matchmaking for pro/early buyers:
-
-router.get("/premium", jwtMw, requireRole, getMatchesController);
+router.get("/premium",requireAuth,requireRole, getMatchesController);
 
 export default router;
